554e14bf4c8d21d41a8751cf0e2feaf4313ea084
sammy <sammy@sammykingx.com.ng>
2023-04-07 01:37:31 +0000

    check error fix

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,7 +2,7 @@
 -- ranked by their longevity
 -- Column names must be: band_name & lifespan
 
-SELECT band_name, ifnull(split, 2020)-ifnull(formed, 0) AS lifespan
+SELECT band_name, ifnull(split, 2020) -formed) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
-ORDER BY lifespan DESC
+ORDER BY lifespan DESC;
