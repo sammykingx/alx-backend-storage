e218585e18368e103ffc8ded7fd696302636e6d3
sammy <sammy@sammykingx.com.ng>
2023-04-11 21:02:12 +0000

    completed task 10

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Module for using PyMongo """
+
+
+def update_topics(mongo_collection, name, topics):
+    """function that changes all topics of a school
+    document based on name"""
+    mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
