210433fa8457915afd6d9d1bf48b5df23c58d781
sammy <sammy@sammykingx.com.ng>
2023-04-13 03:25:56 +0000

    method for getting and setting in class

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -23,3 +23,20 @@ class Cache:
         self._key = str(uuid4())
         self._redis.set(self._key, data)
         return self._key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> UnionOfTypes:
+        """
+        Retrieves data stored in redis using a key
+        converts the result/value back to the desired format
+        """
+        value = self._redis.get(key)
+        return fn(value) if fn else value
+
+    def get_str(self, value: str) -> str:
+        """ get a string """
+        return self.get(self._key, str)
+
+    def get_int(self, value: str) -> int:
+        """ get an int """
+        return self.get(self._key, int)
