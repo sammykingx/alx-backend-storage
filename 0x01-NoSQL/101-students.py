434747ace33f5f35455aa36b021ff873ad67d3ce
sammykingx <sammy@sammykingx.com.ng>
2023-04-14 06:14:32 -0700

    completed task 14

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+""" Module for using PyMongo """
+
+
+def top_students(mongo_collection):
+    """ Returns all students sorted by average score"""
+    top_student = mongo_collection.aggregate([
+        {
+            "$project": {
+                "name": "$name",
+                "averageScore": {"$avg": "$topics.score"}
+            }
+        },
+        {"$sort": {"averageScore": -1}}
+    ])
+
+    return top_student
